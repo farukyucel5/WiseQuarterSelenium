6342c9f1ecd24a26d366140604131a2eaf092ada
Faruk Yucel <farukyucel5@gmail.com>
2022-12-22 17:26:28 +0300

    updating

---
+++ src/test/java/Day02_locators/C01_Byname.java
@@ -0,0 +1,32 @@
+package Day02_locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C01_Byname {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","src/drivers/chromedriver.exe");
+
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        driver.get("https://www.amazon.com");
+
+        WebElement searchBox=driver.findElement(By.name("field-keywords"));
+        searchBox.sendKeys("Nutella " + Keys.ENTER);
+
+        Thread.sleep(2000);
+
+
+
+
+
+        driver.close();
+    }
+}
